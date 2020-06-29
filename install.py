e899f660e106f91b786da30c21ee4f07c5e6e572
OSPeople <62840755+d2nekomet@users.noreply.github.com>
2020-06-29 22:47:13 +0300

    v  1.7.9.1
    Add English translate

--- install.py
+++ install.py
@@ -17,23 +17,24 @@ My Project: t.me/www_ptoject
 '''+colorNum[0] +'''Меню:
 '''+colorNum[0]+'''1)'''+colorNum[1]+''' Debina (Ubuntu) install
 '''+colorNum[0]+'''2)'''+colorNum[1]+''' Termux install
-'''+colorNum[0]+'''3)'''+colorNum[1]+''' Ручная установка\n''')
-	selectMode = input('Выберите нужный вариант: ')
+'''+colorNum[0]+'''3)'''+colorNum[1]+''' Ручная установка (Manual installation)\n''')
+	selectMode = input('Выберите нужный вариант (Select an option): ')
 	if selectMode == '1':
 		os.system('clear')
-		print('Установка началась!')
+		print('Установка началась!\n'+
+			'The installation began')
 		os.system('sudo apt install python3-pip -y')
 		os.system('pip3 install requests')
-		os.system('pip3 install fake_useragent')
 		os.system('pip3 install telegraph')
 		os.system('pip3 install shodan')
 		os.system('clear')
-		print('Чтобы запустить скрипт напишите python3 main.py')
+		print('Чтобы запустить скрипт напишите python3 main.py\n'+
+			'To run the script write to python3 main.py')
 	elif selectMode == '2':
 		os.system('clear')
-		print('Установка началась!')
+		print('Установка началась!\n'+
+			'The installation began')
 		os.system('pip3 install requests')
-		os.system('pip3 install fake_useragent')
 		os.system('pip3 install shodan')
 		os.system('pip3 install telegraph')
 		os.system('clear')
@@ -44,14 +45,12 @@ My Project: t.me/www_ptoject
 Для Debina/Ubuntu:
 1) sudo apt install python3-pip
 2) pip3 install requests
-3) pip3 install fake_useragent
-4) pip3 install shodan
-5) pip3 install telegraph
-Для Termux:
-1) pip3 install requests
-2) pip3 install fake_useragent
 3) pip3 install shodan
 4) pip3 install telegraph
+Для Termux:
+1) pip3 install requests
+2) pip3 install shodan
+3) pip3 install telegraph
 ''')
 
 startInstall()
